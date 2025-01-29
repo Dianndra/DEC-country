efa817f035b34c6f0128aa4c36dab49b5fc15493
Dianndra Oyemen <162504928+Dianndra@users.noreply.github.com>
2025-01-29 00:39:11 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -25,3 +25,57 @@ WHERE "Currency_name" LIKE '%Euro%';
 SELECT COUNT(*)
 FROM country_data
 WHERE "Sub_region" = 'Western Europe';
+
+-- How many country has not yet gain independence
+SELECT COUNT(*)
+FROM country_data
+WHERE "Independence" is False;
+
+-- How many distinct continent and how many country from each
+SELECT "Continents", COUNT("Country_name")
+FROM country_data
+GROUP BY "Continents";
+
+SELECT  COUNT(DISTINCT "Continents") 
+FROM country_data;
+
+-- How many country whose start of the week is not Monday
+SELECT COUNT("Start_Of_Week") 
+FROM country_data
+WHERE "Start_Of_Week" != 'monday';
+
+-- How many countries are not a United Nation member
+SELECT COUNT(*)
+FROM country_data
+WHERE "United_Nation_members" is False;
+
+-- How many countries are United Nation member
+SELECT COUNT(*)
+FROM country_data
+WHERE "United_Nation_members" is True;
+
+-- Least 2 countries with the lowest population for each continents
+SELECT "Country_name", "Population"
+FROM country_data
+ORDER BY "Population" ASC
+LIMIT 2;
+
+-- Top 2 countries with the largest Area for each continent
+WITH temp_table AS 
+	(SELECT "Country_name", "Continents", "Area", ROW_NUMBER() OVER(PARTITION BY "Continents" ORDER BY "Area" DESC) AS row_num
+	FROM country_data)
+SELECT *
+FROM temp_table
+WHERE row_num < 3;
+
+-- Top 5 countries with the largest Area
+SELECT "Country_name", "Area"
+FROM country_data
+ORDER BY "Area" DESC
+LIMIT 5;
+
+-- Top 5 countries with the lowest Area
+SELECT "Country_name", "Area"
+FROM country_data
+ORDER BY "Area" ASC
+LIMIT 5;
